f0e1ac6c2d65d5f9a7fccc14759624fae3aebb62
Yuri <yurijserrano@gmail.com>
2016-01-24 21:55:10 -0200

    Raw_Input

---
+++ programa2.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+
+
+
+"""
+
+Entrada de Dados
+
+RAW_INPUT 
+
+
+Ele recebe uma entrada do usuário e essa entrad automaticamente
+é convertida em uma string podendo ser convertida para os demais
+tipos.
+
+
+"""
+
+
+numero = raw_input("Digite um valor: ")
+print numero
