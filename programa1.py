a921c3e0bbb09389c01d3c2652e9dd2cb2a237cb
Yuri <yurijserrano@gmail.com>
2016-01-24 21:46:29 -0200

    Utilizando input

---
+++ programa1.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+
+
+
+"""
+
+Entrada de Dados
+
+INPUT 
+
+
+Ele recebe uma entrada do usuário, mas somente números
+, ou seja, caso entre um texto aparecerá um erro.
+
+
+"""
+
+
+
+numero = input("Digite um valor:")
+print 'O valor digitado foi -> ', numero
